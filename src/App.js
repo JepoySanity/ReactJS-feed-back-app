f239ce1826b4d378e913c871214b4b6d666d437f
Jepoy Factoran <factoran20@gmail.com>
2022-05-22 20:22:57 +0800

    added css for the feedback ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Header from "./components/Header";
 function App() {
-  <div className="container">
-    <h1>my app</h1>
-  </div>;
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>my app</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;

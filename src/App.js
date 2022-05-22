9a473e0a0a14534d360cdf6dd335c16acd57fb8e
Jepoy Factoran <factoran20@gmail.com>
2022-05-22 22:29:23 +0800

    added the App state feedback

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <>
       <Header />
       <div className="container">
-        <h1>my app</h1>
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   );

bfa9fcf712fe137c9a9ba0ffa13961d9b30fce5f
Jepoy Factoran <factoran20@gmail.com>
2022-05-22 20:32:25 +0800

    added initial styling for feedback UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import PropTypes from "prop-types";
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
+  const headerStyle = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
   return (
-    <header>
+    <header style={headerStyle}>
       <div className="container">
         <h2>{text}</h2>
       </div>
@@ -10,10 +14,15 @@ function Header({ text }) {
 }
 
 Header.defaultProps = {
-  text: "feedback ui",
+  text: "Feedback UI",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
 };
 
 Header.propTypes = {
   text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
 };
+
 export default Header;

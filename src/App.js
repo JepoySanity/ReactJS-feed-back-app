32be9af7be7d97956c19db08e2a705d620b69ed0
Jepoy Factoran <factoran20@gmail.com>
2022-05-28 21:09:34 +0800

    added about page link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -37,6 +38,7 @@ function App() {
                     feedback={feedback}
                     handleDelete={deleteFeedback}
                   />
+                  <AboutIconLink />
                 </>
               }
             ></Route>
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,12 @@
 import Card from "../components/shared/Card";
+import { Link } from "react-router-dom";
 function AboutPage() {
   return (
     <Card>
       <div className="about">
         <h3>This is the about page</h3>
         <p>This is a react app to leave feedback for a product or service</p>
+        <Link to="/">back to home page</Link>
       </div>
     </Card>
   );

6743edfb6dffc750db5bce96d64464cbbb5dfdbb
Jepoy Factoran <factoran20@gmail.com>
2022-05-28 20:40:55 +0800

    added routing for the about page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+import Card from "../components/shared/Card";
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h3>This is the about page</h3>
+        <p>This is a react app to leave feedback for a product or service</p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;

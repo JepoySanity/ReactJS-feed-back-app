fcf136b4980b861c53c8575ee477bf4cfc4fe4d1
Jepoy Factoran <factoran20@gmail.com>
2022-05-30 08:22:20 +0800

    initialize edit state

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,13 +1,14 @@
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaEdit } from "react-icons/fa";
 import { useContext } from "react";
 import { FeedbackContext } from "../context/FeedbackContext";
 import PropTypes from "prop-types";
 import Card from "./shared/Card";
 function FeedbackItem({ item }) {
-  const { deleteFeedback } = useContext(FeedbackContext);
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      {/* delete feedback */}
       <button
         onClick={() => {
           deleteFeedback(item.id);
@@ -15,6 +16,15 @@ function FeedbackItem({ item }) {
         className="close"
       >
         <FaTimes color="#2f2f2f" />
+      </button>
+      {/* edit feedback */}
+      <button
+        onClick={() => {
+          editFeedback(item);
+        }}
+        className="edit"
+      >
+        <FaEdit color="#2f2f2f" />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 export const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
+  //feedback state
   const [feedback, setFeedback] = useState([
     {
       id: 1,
@@ -11,12 +12,24 @@ export const FeedbackProvider = ({ children }) => {
       rating: 10,
     },
   ]);
-
+  //edit feedback state
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+  //add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
   };
-
+  //edit feedback
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+  //delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm("are you sure you want to delete?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
@@ -28,6 +41,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         addFeedback,
+        editFeedback,
         deleteFeedback,
       }}
     >
